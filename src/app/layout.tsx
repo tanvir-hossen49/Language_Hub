bba46741fd77a6b4de494130f58c568b125e7d3f
Golam Aziz <121636105+tanvir-hossen49@users.noreply.github.com>
2024-03-22 06:52:01 +0000

    added home page banner and butttons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,25 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Icon from "./favicon.png";
+import localFont from "next/font/local";
 
-const inter = Inter({ subsets: ["latin"] });
+const duolingoFont = localFont({
+  src: [
+    {
+      path: "../../public/fonts/duolingoFont3.woff2",
+      weight: "700",
+    },
+    {
+      path: "../../public/fonts/duolingoFont1.woff2",
+      weight: "700",
+    },
+    {
+      path: "../../public/fonts/duolingoFont2.woff2",
+      weight: "300",
+    },
+  ],
+  variable: "--font-duolingo",
+});
 
 export const metadata: Metadata = {
   title: "Language_Hub - World best learning platform",
@@ -20,7 +37,9 @@ export default function RootLayout({
       <head>
         <link rel="icon" href={Icon.src} />
       </head>
-      <body className={inter.className}>{children}</body>
+      <body className={`${duolingoFont.variable} font-duolingo`}>
+        {children}
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,77 @@
 import Image from "next/image";
+import Link from "next/link";
 import { Button } from "./components";
-import localFont from "next/font/local";
-
-const myFont = localFont({
-  src: [
-    {
-      path: "../../public/font/duolingoFont1.woff2",
-      weight: "700",
-    },
-  ],
-  variable: "--font-roboto-mono",
-});
 
 export default function Home() {
   return (
-    <header className="mx-[187px] h-[70px] flex items-center justify-between">
-      <div>
-        <Image
-          src={`/banner/duolingo.svg`}
-          alt="duolingo"
-          width="179"
-          height="42"
-        />
-      </div>
-      <div>
-        <Button
-          bgColor="bg-[#58cc02]"
-          textColor="text-white"
-          className="text-[15px] font-bold"
-        >
-          Get started
-        </Button>
-      </div>
-    </header>
+    <>
+      <header className="md:mx-[187px] mx-[50px] md:h-[70px] h-[50px]  flex items-center sm:justify-between justify-center">
+        <div>
+          <Image
+            src={`/banner/duolingo.svg`}
+            alt="duolingo"
+            width="179"
+            height="42"
+          />
+        </div>
+
+        <div className="text-[#afafaf] font-[600] text-[17px] hidden sm:block">
+          <button className="cursor-pointer select-none flex gap-2 items-center ">
+            SITE LANGUAGE: ENGLISH
+            <Image
+              src="https://d35aaqx5ub95lt.cloudfront.net/images/splash/c6eae48dd48246c89e415b89f9e55282.svg"
+              alt="duolingo"
+              className="-mt-1"
+              width="17"
+              height="17"
+            />
+          </button>
+        </div>
+      </header>
+
+      <main className="md:mx-[187px] mx-[50px] py-12 ">
+        <div className="md:flex gap-10 items-center justify-between">
+          <div className="w-full md:w-1/2">
+            <Image
+              src={`/banner/duolingo1.png`}
+              alt="duolingo"
+              className="mx-auto"
+              width="450"
+              height="450"
+            />
+          </div>
+
+          <div className="w-full md:w-1/2 text-center">
+            <h1 className="text-[#4b4b4b] font-[700] text-[28px]">
+              The free, fun, and effective way to learn a language!
+            </h1>
+
+            <div className="flex flex-col gap-4 mt-10 ">
+              <Link href="#">
+                <Button
+                  bgColor="bg-[#58cc02]"
+                  textColor="text-white"
+                  className="border-b-4 w-full md:w-3/4 border-[#58a700] relative text-[15px] uppercase font-[700] letter-spacing-[0.5px] tracking-[0.5px] px-5 hover:bg-[#60e000]"
+                  font-duolingo-700
+                >
+                  Get started
+                </Button>
+              </Link>
+
+              <Link href="#">
+                <Button
+                  bgColor="bg-white"
+                  textColor="text-[#1cb0f6]"
+                  className="border-2 border-b-4 w-full md:w-3/4 border-gray-300 relative text-[15px] uppercase font-[700] letter-spacing-[0.5px] tracking-[0.5px] px-5 hover:bg-gray-200"
+                  font-duolingo-700
+                >
+                  I ALREADY HAVE AN ACCOUNT
+                </Button>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </main>
+    </>
   );
 }
